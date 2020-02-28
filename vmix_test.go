3f194777b1d9bb8eb1c4cffe16a3b8fd692ecd27
Flowing <bitoreieru@gmail.com>
2020-02-28 13:21:11 +0900

    Added testing for browser.go

--- vmix_test.go
+++ vmix_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 )
 
+// General vMix features
+// vmix.go or models.go
 func TestRefresh(t *testing.T) {
 	vmix, err := NewVmix("http://localhost:8088")
 	if err != nil {
@@ -15,6 +17,7 @@ func TestRefresh(t *testing.T) {
 	}
 }
 
+// transition.go
 func TestCut(t *testing.T) {
 	vmix, err := NewVmix("http://localhost:8088")
 	if err != nil {
@@ -32,6 +35,95 @@ func TestFade(t *testing.T) {
 		t.Fatal(err)
 	}
 	err = vmix.Fade(nil, 500)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+//browser.go
+func TestBrowserBack(t *testing.T) {
+	vmix, err := NewVmix("http://localhost:8088")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = vmix.BrowserBack(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestBrowserForward(t *testing.T) {
+	vmix, err := NewVmix("http://localhost:8088")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = vmix.BrowserForward(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestBrowserKeyboardDisabled(t *testing.T) {
+	vmix, err := NewVmix("http://localhost:8088")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = vmix.BrowserKeyboardDisabled(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestBrowserKeyboardEnabled(t *testing.T) {
+	vmix, err := NewVmix("http://localhost:8088")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = vmix.BrowserKeyboardEnabled(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestBrowserMouseDisabled(t *testing.T) {
+	vmix, err := NewVmix("http://localhost:8088")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = vmix.BrowserMouseDisabled(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestBrowserMouseEnabled(t *testing.T) {
+	vmix, err := NewVmix("http://localhost:8088")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = vmix.BrowserMouseEnabled(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestBrowserNavigate(t *testing.T) {
+	vmix, err := NewVmix("http://localhost:8088")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = vmix.BrowserNavigate(0, "http://google.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestBrowserReload(t *testing.T) {
+	vmix, err := NewVmix("http://localhost:8088")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = vmix.BrowserReload(0)
 	if err != nil {
 		t.Fatal(err)
 	}
